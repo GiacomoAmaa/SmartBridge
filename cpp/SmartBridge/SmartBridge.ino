eec0f4e752e46a56601db30d1b740cc6252b9a88
DanielPellanda <pellanda.daniel01@gmail.com>
2022-11-27 11:44:18 +0100

    Fixed led blinking and smart lighting bug

--- cpp/SmartBridge/SmartBridge.ino
+++ cpp/SmartBridge/SmartBridge.ino
@@ -41,6 +41,8 @@ void printAlarmState() {
 
 void setup(){
     Serial.begin(9600);
+    Serial.println();
+
     int leds[NUM_LEDS];
     state = NORMAL;
 
@@ -70,6 +72,8 @@ void setup(){
     taskmgr.addTask(sonartask);
     taskmgr.addTask(servotask);
     taskmgr.addTask(ledtask);
+
+    Serial.println();
 }
 
 void loop(){
